6a664912fbebff22088e7732db62e441eb2b3257
Mart <mart_lepanen@hotmail.com>
2023-11-07 17:41:46 +0200

    Initial commit
    Bootstrapped with create react app, removed the not necessary stuff

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Yay</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
